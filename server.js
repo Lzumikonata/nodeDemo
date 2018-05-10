06681bed0bdc37482439fb35492bcff5da68f2e3
Lzumikonata <504048655@qq.com>
2018-05-10 16:42:45 +0800

    node

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
+// const express = require('express')
+// const fs  = require("fs")
+// const dbConfig = require('./db/DBConfig')
+// const UserSQL = require('./db/usersql')
+// const pool = mysql.createPool(dbConfig.mysql)
+// console.log(UserSQL.insert)
+const app = require('./application')
+  const server = app.listen(8081, () => {
+    let host = server.address().address
+    let port = server.address().port
+    console.log('Example app listening at http://%s:%s', host, port);
+  })
 
-const express = require('express')
-const app = express()
-const fs = require("fs")
 
-app.all('*', function(req, res, next) { //设置请求头
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By",' 3.2.1');
-  res.header("Content-Type", "application/json;charset=utf-8");
-  next()
-})
-
-app.get('/users_list', (req, res) => {   //这个就是我们的接口了
-  const jsonData = fs.readFileSync( __dirname + '/' + 'users.json', 'utf-8')//同步读取文件
-  res.status(200)
-  res.json(jsonData)
-});
-
-var server = app.listen(8081, () => {
-  let host = server.address().address
-  let port = server.address().port
-  console.log('Example app listening at http://%s:%s', host, port);
-})
 
