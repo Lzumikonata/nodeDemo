39b64d48a1ccf1e46be1b737af3eb7f9df6b75cb
Lzumikonata <504048655@qq.com>
2018-05-10 18:22:58 +0800

    read file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const sessionController = require('../app/controller/index')
+const fs  = require("fs")
+const path = require('path')
+const url = path.resolve('users.json')
 module.exports = app => {
   app.post('/user_name',(req, res) => {
-  const result = { 'name': req.body.name, 'uid': '2' }
+ // const result = { 'name': req.body.name, 'uid': '2' }
+  const file =fs.readFileSync(url, 'utf-8')
   res.status(200)
-  res.json(result)
+  res.json(file)
 })
   app.get('/users_list', (req, res) => {sessionController.getList(req, res)})
 }
