f45f0ed74eaf1c0a16719eff12454c605775733f
Lzumikonata <504048655@qq.com>
2018-05-11 14:34:58 +0800

    fixed

---
+++ ecosystem.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'process-0',
+      script    : 'server.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+
+    // Second application
+    {
+      name      : 'process-1',
+      script    : 'aaa.js'
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'wuke',
+      host : '127',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'node',
+      host : '120.79.92.83',
+      ref  : 'origin/master',
+      repo : 'git@github.com:Lzumikonata/nodeDemo.git',
+      path : '/home/wuke/server/nodeDemo',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
